a7d5fea272a082b79cdf6800ab40e7bd8070621e
torklo <tord.kloster@knowit.no>
2020-02-06 09:18:41 +0100

    fix formatting

--- src/main/kotlin/no/ok/origo/dataplatform/commons/Utils.kt
+++ src/main/kotlin/no/ok/origo/dataplatform/commons/Utils.kt
@@ -1,8 +1,6 @@
 package no.ok.origo.dataplatform.commons
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import org.slf4j.LoggerFactory
 
